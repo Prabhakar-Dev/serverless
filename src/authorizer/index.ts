e458657a1a5ba2dd51c63aa63ffc9c8c15f7433b
mayank-successive <mayank.awasthi@successive.tech>
2024-01-03 14:26:33 +0530

    add custom authorizer

--- src/authorizer/index.ts
+++ src/authorizer/index.ts
@@ -1,30 +1,36 @@
 import * as jwt from 'jsonwebtoken';
 import { CONSTANTS } from '../common/constants'
 import { getLogger } from 'common/logger';
+import { CustomAPIGatewayProxyEvent } from './types';
+import { APIGatewaySimpleAuthorizerResult } from 'aws-lambda';
 
 const logger = getLogger(__filename);
 
 // Environment Variables!
 const JWT_SECRET_KEY = process.env.JWT_SECRET || '';
-const JWT_EXPIRY = process.env.JWT_TOKEN_EXPIRY_IN_MINS;
 
-const verifyToken = async (token: string): Promise<Record<string, any>> => {
+const verifyToken = async (token: string): Promise<Record<string, number | number | string | unknown>> => {
   try {
-    const decoded = await jwt.verify(token, JWT_SECRET_KEY) as Record<string, any>;
+    const decoded = await jwt.verify(token, JWT_SECRET_KEY) as Record<string, number | string | unknown>;
     return decoded;
   } catch (error) {
     throw new Error(CONSTANTS.AUTHORIZATION_TOKEN_INVALID);
   }
 };
 
-export const handler = async (event: any) => {
+export const handler = async (event: CustomAPIGatewayProxyEvent): Promise<APIGatewaySimpleAuthorizerResult> => {
   try {
+    console.log('EVENT DATA', event)
+
+    // return { isAuthorized: true };
     logger.info('authorizer', {
       step: 'init',
-      event
+      event: JSON.stringify(event),
     })
 
-    const authorizationHeader = event.headers?.Authorization;
+    const authorizationHeader = event.authorizationToken;
+    console.log('authorizationHeader:: ', authorizationHeader);
+    
     if (!authorizationHeader) {
       logger.error('authorizer', {
         step: 'token missing in header',
@@ -48,21 +54,30 @@ export const handler = async (event: any) => {
       step: 'end',
       decodedToken
     });
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        principalId: Math.random()
-      }),
-    };
-   
+    return generatePolicy(decodedToken.iat, 'Allow', event.methodArn);
   } catch (error) {
     logger.error('authorizer', {
       step: 'error',
       error: error.message
     })
-    return {
-      statusCode: error.status || CONSTANTS.STATUS_CODE.INTERNAL_SERVER_ERROR,
-      body: JSON.stringify({ error: error.message }),
-    };
+    return { statusCode: 401, error: error.message, ...generatePolicy(0,'',''), };
   }
+};
+
+
+
+const generatePolicy = (principalId: any , effect: string, resource: string) => {
+  const authResponse: any = {
+      principalId,
+      policyDocument: {
+          Statement: [
+              {
+                  Action: 'execute-api:Invoke',
+                  Effect: effect,
+                  Resource: resource,
+              },
+          ],
+      },
+  };
+  return authResponse;
 };
